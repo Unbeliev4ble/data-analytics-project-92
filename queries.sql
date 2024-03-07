6ebbe4ae3cd03b7612e6f6aeb890bf2d7d9a6032
Andrey Volkov <141320245+Unbeliev4ble@users.noreply.github.com>
2024-03-07 10:19:36 +0300

    linter

--- queries.sql
+++ queries.sql
@@ -1,73 +1,58 @@
 --count of unique customers
 
-select count(customer_id) as customers_count
-from customers c 
-;
-
-
-
+select count(c.customer_id) as customers_count
+from customers c;
 
 --sellers with less than avg income
 
 with tab as (
-	select 
-	*,
-	s.quantity * p.price as income,
-	concat_ws(' ', e.first_name, e.last_name) as seller_full
-	from sales s
-	left join employees e ON s.sales_person_id = e.employee_id
-	left join products p on s.product_id = p.product_id)
-
 select 
+    *,
+    s.quantity * p.price as income,
+    concat_ws(' ', e.first_name, e.last_name) as seller_full
+from sales s
+    left join employees on s.sales_person_id = e.employee_id
+    left join products p on s.product_id = p.product_id)
+
+select
 	seller_full as seller,
 	floor(avg(income)) as average_income
 from tab
 group by seller_full
 having avg(income) < (
-	select 
-	avg(income)
-	from tab
-	)
-order by average_income
-;
- 
- 
-
-
+        select 
+        avg(income)
+        from tab
+        )
+order by average_income;
 
 --top 10 sellers by income
 
 with tab as (
-	select *,
-	concat_ws(' ', e.first_name, e.last_name) as seller_full
-	from sales s
-	left join employees e on s.sales_person_id = e.employee_id
-	left join products p on s.product_id = p.product_id
-	)
-
-select 
-	seller_full as seller,
+    select *,
+        concat_ws(' ', e.first_name, e.last_name) as seller_full
+    from sales s
+    left join employees e on s.sales_person_id = e.employee_id
+    left join products p on s.product_id = p.product_id
+        )
+select
+    seller_full as seller,
 	count(sales_id) as operations,
 	floor(sum(price * quantity)) as income
 from tab
 group by seller_full
 order by income desc
-limit 10
-;
-
-
-
-
+limit 10;
 
 -- sales by day of week
 
 with dow_income as (
-	select
-		concat_ws(' ', e.first_name, e.last_name) as seller_full,
-		extract(dow from s.sale_date + 6) as n_day_of_week,
-		to_char(s.sale_date, 'day') as day_of_week,
-		s.quantity * p.price as income
-	from sales s
+    select
+        concat_ws(' ', e.first_name, e.last_name) as seller_full,
+        extract(isodow from s.sale_date) as n_day_of_week,
+        to_char(s.sale_date, 'day') as day_of_week,
+        s.quantity * p.price as income
+    from sales s
 	left join employees e on s.sales_person_id = e.employee_id
 	left join products p on s.product_id = p.product_id
 	)
@@ -78,13 +63,7 @@ select
 	floor(sum(income)) as income
 from dow_income
 group by day_of_week, seller_full, n_day_of_week
-order by n_day_of_week
-;
-
-
-
-
-
+order by n_day_of_week;
 
 -- customers age groups
 
@@ -94,37 +73,33 @@ select
 		when age between 26 and 40 then '26-40'
 		when age > 40 then '40+'
 	end as age_category,
-count(customer_id)	
+count(c.customer_id) age_count
 from customers c
 group by age_category
-order by age_category
-;
-
+order by age_category;
 
 -- unique buyers by months
 
 select
 	to_char(s.sale_date, 'YYYY-MM') as selling_month,
-	count(distinct s.customer_id) as total_customers,
-	floor(sum(p.price * s.quantity)) as income
+    count(distinct s.customer_id) as total_customers,
+    floor(sum(p.price * s.quantity)) as income
 from sales s
-left join products p using(product_id)
+left join products p on p.product_id = s.product_id
 group by selling_month
-order by selling_month
-;
-
+order by selling_month;
 
 --first purchase for 0 customers
 
 with full_names as (
 	select 
-		*,
-		concat_ws(' ', c.first_name, c.last_name) as customer,
-		concat_ws(' ', e.first_name, e.last_name) as seller,
-		p.price * s.quantity as income
-	from sales s
-	left join customers c using(customer_id)
-	left join products p using(product_id)
+    *,
+    concat_ws(' ', c.first_name, c.last_name) as customer,
+    concat_ws(' ', e.first_name, e.last_name) as seller,
+    p.price * s.quantity as income
+    from sales s
+	left join customers c on c.customer_id = s.customer_id
+	left join products p on p.product_id = s.product_id
 	left join employees e on e.employee_id = s.sales_person_id
     ),
 rowed as (
@@ -135,20 +110,13 @@ rowed as (
 		seller,
 		sale_date,
 		income,
-		row_number() over(partition by customer order by income)
+		row_number() over (partition by customer order by sale_date)
 from full_names
 	)
-
-select customer, sale_date, seller
+select 
+    customer,
+    sale_date,
+    seller
 from rowed
 where row_number = 1 and income = 0
-order by customer_id
-;
-
-
-
-
-
-
-
-
+order by customer_id;
